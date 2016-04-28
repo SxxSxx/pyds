d80c9c54f354c6ea278968eb432a4c1c34422dec
SxxSxx <1467353814@qq.com>
2016-04-28 23:29:20 +0800

    first change

--- pyds_test.py
+++ pyds_test.py
@@ -16,23 +16,23 @@ class PyDSTest(unittest.TestCase):
         self.m2 = MassFunction({'b':0.5, 'c':0.2, 'ac':0.3, 'a':0.0})
         self.m3 = MassFunction({():0.4, 'c':0.2, 'ac':0.3, 'ab':0.1}) # unnormalized mass function
         random.seed(0) # make tests deterministic
-    
+
     def _assert_equal_belief(self, m1, m2, places=8):
         for h in m1.focal() | m2.focal():
             self.assertAlmostEqual(m1[h], m2[h], places)
-    
+
     def test_init(self):
         """Test equivalence of different mass function initialization methods."""
         m1 = MassFunction([(('a',), 0.4), (('b',), 0.2), (('a', 'd'), 0.1), (('a', 'b', 'c', 'd'), 0.3)])
         self.assertEqual(self.m1, m1)
         m1 = MassFunction([('a', 0.4), ('b', 0.2), ('ad', 0.1), ('abcd', 0.3)])
         self.assertEqual(self.m1, m1)
-    
+
     def test_items(self):
         self.assertEqual(0.0, self.m1['x'])
         self.m1['ad'] += 0.5
         self.assertEqual(0.6, self.m1['ad'])
-    
+
     def test_copy(self):
         c = self.m1.copy()
         for k in self.m1.keys():
@@ -40,12 +40,12 @@ class PyDSTest(unittest.TestCase):
         c['a'] = 0.3
         # assert that the original object remains unchanged
         self.assertEqual(0.4, self.m1['a'])
-    
+
     def test_del(self):
         del self.m1['a']
         self.assertEqual(3, len(self.m1))
         self.assertEqual(0.0, self.m1['a'])
-    
+
     def test_bel(self):
         # compute the belief of a single hypothesis
         self.assertEqual(0.4, self.m1.bel('a'))
@@ -64,12 +64,12 @@ class PyDSTest(unittest.TestCase):
         self.assertEqual(8, len(bel3))
         for h, v in bel3.items():
             self.assertEqual(self.m3.bel(h), v)
-    
+
     def test_from_bel(self):
         self._assert_equal_belief(self.m1, MassFunction.from_bel(self.m1.bel()), 8)
         self._assert_equal_belief(self.m2, MassFunction.from_bel(self.m2.bel()), 8)
         self._assert_equal_belief(self.m3, MassFunction.from_bel(self.m3.bel()), 8)
-    
+
     def test_pl(self):
         # compute the plausibility of a single hypothesis
         self.assertEqual(0.8, self.m1.pl('a'))
@@ -88,12 +88,12 @@ class PyDSTest(unittest.TestCase):
         self.assertEqual(8, len(pl3))
         for h, v in pl3.items():
             self.assertEqual(self.m3.pl(h), v)
-    
+
     def test_from_pl(self):
         self._assert_equal_belief(self.m1, MassFunction.from_pl(self.m1.pl()), 8)
         self._assert_equal_belief(self.m2, MassFunction.from_pl(self.m2.pl()), 8)
         self._assert_equal_belief(self.m3, MassFunction.from_pl(self.m3.pl()), 8)
-    
+
     def test_q(self):
         # compute the commonality of a single hypothesis
         self.assertEqual(0.8, self.m1.q('a'))
@@ -112,12 +112,12 @@ class PyDSTest(unittest.TestCase):
         self.assertEqual(8, len(q3))
         for h, v in q3.items():
             self.assertEqual(self.m3.q(h), v)
-    
+
     def test_from_q(self):
         self._assert_equal_belief(self.m1, MassFunction.from_q(self.m1.q()), 8)
         self._assert_equal_belief(self.m2, MassFunction.from_q(self.m2.q()), 8)
         self._assert_equal_belief(self.m3, MassFunction.from_q(self.m3.q()), 8)
-    
+
     def test_condition(self):
         m1 = self.m1.condition('ad')
         self.assertEqual(0.5, m1['a'])
@@ -134,7 +134,7 @@ class PyDSTest(unittest.TestCase):
         self.assertAlmostEqual(0.6, m3_un[set()])
         self.assertEqual(0.3, m3_un['a'])
         self.assertEqual(0.1, m3_un['ab'])
-    
+
     def test_combine_conjunctive(self):
         def test(m, empty_mass, places=10):
             self.assertAlmostEqual(empty_mass, m[set()], places)
@@ -157,7 +157,7 @@ class PyDSTest(unittest.TestCase):
         self._assert_equal_belief(m_single, m_multi, 10)
         # combine incompatible mass function
         self.assertFalse(self.m1 & MassFunction({(0, 1):0.8, (0,):0.2}))
-    
+
     def test_combine_disjunctive(self):
         def test(m, places):
             self.assertAlmostEqual(0.2, m['ab'], places)
@@ -175,7 +175,7 @@ class PyDSTest(unittest.TestCase):
         m_multi = self.m1.combine_disjunctive([self.m1, self.m2])
         for h, v in m_single.items():
             self.assertAlmostEqual(v, m_multi[h])
-    
+
     def test_weight_function(self):
         """
         Examples from:
@@ -200,7 +200,7 @@ class PyDSTest(unittest.TestCase):
         self.assertAlmostEqual(1.0, w2[frozenset('c')])
         self.assertAlmostEqual(1.0, w2[frozenset('ac')])
         self.assertAlmostEqual(3./7., w2[frozenset('bc')])
-    
+
     def test_combine_cautious(self):
         """
         Examples from:
@@ -218,12 +218,12 @@ class PyDSTest(unittest.TestCase):
         self.assertAlmostEqual(0.0, m['ac'])
         self.assertAlmostEqual(0.2, m['bc'])
         self.assertAlmostEqual(0.08, m['abc'])
-    
+
     def test_conflict(self):
         self.assertAlmostEqual(-log(0.55), self.m1.conflict(self.m2), 8);
         self.assertAlmostEqual(-log(0.55), self.m1.conflict(self.m2, sample_count=10000), 1);
         self.assertEqual(float('inf'), self.m1.conflict(MassFunction({'e': 1})));
-    
+
     def test_normalize(self):
         v = self.m1['a']
         del self.m1['a']
@@ -232,14 +232,14 @@ class PyDSTest(unittest.TestCase):
         self.assertAlmostEqual(0.1 / (1 - v), self.m1['ad'])
         self.assertAlmostEqual(0.3 / (1 - v), self.m1['abcd'])
         self.assertEqual(0, len(MassFunction().normalize()))
-    
+
     def test_multiple_dimensions(self):
         md1 = MassFunction({(('a', 1), ('b', 2)): 0.8, (('a', 1),):0.2})
         md2 = MassFunction({(('a', 1), ('b', 2), ('c', 1)): 1.0})
         md12 = md1 & md2
         self.assertAlmostEqual(0.2, md12[{('a', 1)}])
         self.assertAlmostEqual(0.8, md12[{('a', 1), ('b', 2)}])
-    
+
     def test_map(self):
         # vacuous extension
         frame = {1, 2}
@@ -249,7 +249,7 @@ class PyDSTest(unittest.TestCase):
         # projection
         projected = extended.map(lambda h: (t[0] for t in h))
         self.assertEqual(self.m3, projected)
-    
+
     def test_pignistic(self):
         p1 = self.m1.pignistic()
         self.assertEqual(0.525, p1['a'])
@@ -260,7 +260,7 @@ class PyDSTest(unittest.TestCase):
         self.assertEqual(0.2 / 0.6, p3['a'])
         self.assertEqual(0.05 / 0.6, p3['b'])
         self.assertEqual(0.35 / 0.6, p3['c'])
-    
+
     def test_local_conflict(self):
         c = 0.5 * log(1 / 0.5, 2) + 0.2 * log(1 / 0.2, 2) + 0.3 * log(2 / 0.3, 2)
         self.assertEqual(c, self.m2.local_conflict())
@@ -268,10 +268,10 @@ class PyDSTest(unittest.TestCase):
         # pignistic entropy
         h = -0.125 * log(0.125, 2) - 0.075 * log(0.075, 2) - 0.275 * log(0.275, 2) - 0.525 * log(0.525, 2)
         self.assertAlmostEqual(h, self.m1.pignistic().local_conflict())
-    
+
     def test_hartley_measure(self):
         self.assertEqual(0.1 + 0.3 * log(4, 2), self.m1.hartley_measure())
-    
+
     def test_norm(self):
         self.assertEqual(0, self.m1.norm(self.m1))
         self.assertEqual(0, self.m1.norm(self.m1, p = 1))
@@ -284,7 +284,7 @@ class PyDSTest(unittest.TestCase):
         pruned = self.m2.prune()
         self.assertFalse('a' in pruned)
         self._assert_equal_belief(self.m2, pruned, 10)
-    
+
     def test_sample(self):
         sample_count = 1000
         samples_ran = self.m1.sample(sample_count, quantization=False)
@@ -295,7 +295,7 @@ class PyDSTest(unittest.TestCase):
             self.assertAlmostEqual(v, float(samples_ran.count(h)) / sample_count, places=1)
             self.assertAlmostEqual(v, float(samples_ml.count(h)) / sample_count, places=20)
         self.assertEqual(0, len(MassFunction().sample(sample_count)))
-    
+
     def test_markov(self):
         def test(m, places):
             self.assertAlmostEqual(0.4 * 0.8, m[(4, 6)], places)
@@ -309,7 +309,7 @@ class PyDSTest(unittest.TestCase):
         transition_model_mc = lambda s, n: transition_model(s).sample(n)
         test(m.markov(transition_model), 10)
         test(m.markov(transition_model_mc, sample_count=10000), 2)
-    
+
     def test_gbt(self):
         def test(m, places):
             self.assertAlmostEqual(0.3 * 0.8 / (1 - 0.7 * 0.2), m['ab'], places)
@@ -320,15 +320,15 @@ class PyDSTest(unittest.TestCase):
         test(MassFunction.gbt(pl, sample_count=10000), 2)
         pl = {'a':0.3, 'b':0.8, 'c':0.0, 'd':1.0}
         self._assert_equal_belief(MassFunction.gbt(pl), MassFunction.gbt(pl, sample_count=10000), 1)
-    
+
     def test_frame(self):
         self.assertEqual({'a', 'b', 'c', 'd'}, self.m1.frame())
         self.assertEqual({'a', 'b', 'c'}, self.m2.frame())
         self.assertEqual(set(), MassFunction().frame())
-    
+
     def test_singletons(self):
         self.assertSetEqual({frozenset('a'), frozenset('b'), frozenset('c'), frozenset('d')}, self.m1.singletons())
-    
+
     def test_focal(self):
         self.assertEqual(4, len(list(self.m1.focal())))
         for f in self.m1.focal():
@@ -336,7 +336,7 @@ class PyDSTest(unittest.TestCase):
         self.m1[{'b'}] = 0
         self.assertEqual(3, len(self.m1.focal()))
         self.assertFalse({'b'} in self.m1.focal())
-    
+
     def test_core(self):
         self.assertEqual({'a', 'b', 'c', 'd'}, self.m1.core())
         self.m1[{'a', 'b', 'c', 'd'}] = 0
@@ -344,21 +344,21 @@ class PyDSTest(unittest.TestCase):
         self.assertEqual(set(), MassFunction().core())
         # test combined core
         self.assertEqual({'a', 'b'}, self.m1.core(self.m2))
-    
+
     def test_all(self):
         expected = {frozenset(), frozenset('a'), frozenset('b'), frozenset('ab')}
         self.assertSetEqual(expected, set(MassFunction({'a':0.1, 'b':0}).all()))
-    
+
     def test_max_bel(self):
         self.assertEqual(frozenset('a'), self.m1.max_bel())
         self.assertEqual(frozenset('c'), self.m3.max_bel())
         self.assertTrue(MassFunction({('ab'):1}).max_bel() in {frozenset('a'), frozenset('b')})
-    
+
     def test_max_pl(self):
         self.assertEqual(frozenset('a'), self.m1.max_pl())
         self.assertEqual(frozenset('c'), self.m3.max_pl())
         self.assertTrue(MassFunction({('ab'):0.8, 'c':0.2}).max_pl() in {frozenset('a'), frozenset('b')})
-    
+
     def test_combine_gbt(self):
         pl = [('b', 0.8), ('c', 0.5)]
         correct = self.m1.combine_conjunctive(MassFunction.gbt(pl))
@@ -373,17 +373,17 @@ class PyDSTest(unittest.TestCase):
             p_correct[(s,)] = l * p_prior[(s,)]
         p_correct.normalize()
         self._assert_equal_belief(p_correct, p_posterior, 10)
-    
+
     def test_is_probabilistic(self):
         self.assertFalse(self.m1.is_probabilistic())
         self.assertTrue(self.m1.pignistic().is_probabilistic())
         for p in self.m1.sample_probability_distributions(100):
             self.assertTrue(p.is_probabilistic())
-    
+
     def test_sample_probability_distributions(self):
         for p in self.m1.sample_probability_distributions(100):
             self.assertTrue(self.m1.is_compatible(p))
-    
+
     def test_from_possibility(self):
         """
         Example 1 from:
@@ -396,7 +396,7 @@ class PyDSTest(unittest.TestCase):
         self.assertEqual(1 - x, m['a'])
         self.assertEqual(2 * x - 1, m['ab'])
         self.assertEqual(1 - x, m['abc'])
-    
+
     def test_pignistic_inverse(self):
         """
         Example 1 from:
@@ -408,27 +408,27 @@ class PyDSTest(unittest.TestCase):
         self.assertEqual(0.5, m['a'])
         self.assertAlmostEqual(0.2, m['ab'])
         self.assertAlmostEqual(0.3, m['abc'])
-    
+
     def test_to_array_index(self):
         self.assertEqual(0, MassFunction._to_array_index((), ('a', 'b', 'c')))
         self.assertEqual(1, MassFunction._to_array_index(('a',), ('a', 'b', 'c')))
         self.assertEqual(2, MassFunction._to_array_index(('b',), ('a', 'b', 'c')))
         self.assertEqual(4, MassFunction._to_array_index(('c',), ('a', 'b', 'c')))
         self.assertEqual(7, MassFunction._to_array_index(('a', 'b', 'c'), ('a', 'b', 'c')))
-    
+
     def test_from_array_index(self):
         self.assertEqual(frozenset(), MassFunction._from_array_index(0, ('a', 'b', 'c')))
         self.assertEqual(frozenset(('a',)), MassFunction._from_array_index(1, ('a', 'b', 'c')))
         self.assertEqual(frozenset(('b',)), MassFunction._from_array_index(2, ('a', 'b', 'c')))
         self.assertEqual(frozenset(('c',)), MassFunction._from_array_index(4, ('a', 'b', 'c')))
         self.assertEqual(frozenset(('a', 'b', 'c')), MassFunction._from_array_index(7, ('a', 'b', 'c')))
-    
+
     def test_to_array(self):
         self.assertListEqual([0, 0, 0.5, 0.0, 0.2, 0.3, 0, 0], list(self.m2.to_array(('a', 'b', 'c'))))
-    
+
     def test_from_array(self):
         self._assert_equal_belief(self.m1, MassFunction.from_array(self.m1.to_array(self.m1.frame()), self.m1.frame()))
-    
+
     def test_confidence_intervals(self):
         """
         Numbers taken from:
@@ -444,13 +444,13 @@ class PyDSTest(unittest.TestCase):
         for h, p_u in upper.items():
             p = hist[h] / float(sum(hist.values()))
             self.assertAlmostEqual(2. * p - lower[h], p_u, places=1)
-    
+
     def test_from_samples(self):
         """
         Example 1 (maxbel) and example 7 (ordered) from:
         T. Denoeux (2006), "Constructing belief functions from sample data using multinomial confidence regions",
         International Journal of Approximate Reasoning 42, 228-252.
-        
+
         Example 6 (mcd) from:
         A. Aregui, T. Denoeux (2008), "Constructing consonant belief functions from sample data using confidence sets of pignistic probabilities",
         International Journal of Approximate Reasoning 49, 575-594.
@@ -491,11 +491,11 @@ class PyDSTest(unittest.TestCase):
         self.assertAlmostEqual(0.14,  m[range(2, 7)], 2)
         # mcd
         poss = {1: 0.171, 2: 0.258, 3: 0.353, 4: 0.462, 5: 0.688, 6: 0.735, 7: 0.804, 8: 0.867, 9: 0.935, 10: 1.0} # 8: 0.873
-        m = MassFunction.from_samples(failure_mode_data, method='mcd', alpha=0.1) 
+        m = MassFunction.from_samples(failure_mode_data, method='mcd', alpha=0.1)
         self._assert_equal_belief(MassFunction.from_possibility(poss), m, 1)
         # mcd-approximate
         m = MassFunction.from_samples(failure_mode_data, method='mcd-approximate', alpha=0.1)
-        poss = {1: 0.171, 2: 0.258, 3: 0.353, 4: 0.462, 5: 0.688, 6: 0.747, 7: 0.875, 8: 0.973, 9: 1.0, 10: 1.0} 
+        poss = {1: 0.171, 2: 0.258, 3: 0.353, 4: 0.462, 5: 0.688, 6: 0.747, 7: 0.875, 8: 0.973, 9: 1.0, 10: 1.0}
         self._assert_equal_belief(MassFunction.from_possibility(poss), m, 2)
         # bayesian
         m = MassFunction.from_samples(precipitation_data, method='bayesian', s=0)
@@ -506,7 +506,7 @@ class PyDSTest(unittest.TestCase):
         self.assertAlmostEqual(1. / float(sum(precipitation_data.values()) + 1), m[MassFunction._convert(precipitation_data.keys())])
         for e, n in precipitation_data.items():
             self.assertAlmostEqual(n / float(sum(precipitation_data.values()) + 1), m[(e,)])
-    
+
     def test_powerset(self):
         s = range(2)
         p = set(powerset(s))
@@ -516,43 +516,43 @@ class PyDSTest(unittest.TestCase):
         self.assertTrue({1} in p)
         self.assertTrue({0, 1} in p)
         self.assertEqual(2**6, len(list(powerset(range(6)))))
-    
+
     def test_gbt_m(self):
         likelihoods = [('a', 0.3), ('b', 0.8), ('c', 0.0), ('d', 0.5)] # likelihoods as a list
         m = MassFunction.gbt(likelihoods)
         for h, v in m.items():
             self.assertAlmostEqual(v, gbt_m(h, likelihoods), 8)
-    
+
     def test_gbt_bel(self):
         likelihoods = {'a':0.3, 'b':0.8, 'c':0.0, 'd':0.5}
         m = MassFunction.gbt(likelihoods)
         for h in m:
             self.assertAlmostEqual(m.bel(h), gbt_bel(h, likelihoods), 8)
-    
+
     def test_gbt_pl(self):
         likelihoods = {'a':0.3, 'b':0.8, 'c':0.0, 'd':0.5}
         m = MassFunction.gbt(likelihoods)
         for h in m:
             self.assertAlmostEqual(m.pl(h), gbt_pl(h, likelihoods), 8)
-    
+
     def test_gbt_q(self):
         likelihoods = {'a':0.3, 'b':0.8, 'c':0.0, 'd':0.5}
         m = MassFunction.gbt(likelihoods)
         for h in m:
             self.assertAlmostEqual(m.q(h), gbt_q(h, likelihoods), 8)
-    
+
     def test_gbt_pignistic(self):
         likelihoods = {'a':0.3, 'b':0.8, 'c':0.0, 'd':0.5, 'e':1.0}
         p = MassFunction.gbt(likelihoods).pignistic()
         for s in p:
             self.assertAlmostEqual(p[s], gbt_pignistic(tuple(s)[0], likelihoods), 8)
-    
+
     def test_multiplication(self):
         product = 0.5 * self.m1
         for (h, v) in self.m1.items():
             self.assertEqual(0.5 * v, product[h])
         self._assert_equal_belief(0.5 * self.m1, self.m1 * 0.5)
-    
+
     def test_addition(self):
         m_sum = self.m2 + self.m3
         self.assertEqual(0.4, m_sum['c'])
@@ -561,8 +561,8 @@ class PyDSTest(unittest.TestCase):
         self.assertEqual(0.4, m_sum[()])
         self.assertEqual(0.1, m_sum['a', 'b'])
         self.assertEqual(0.0, m_sum['a'])
-        
-        
+
+
 
 if __name__ == "__main__":
     unittest.main()
